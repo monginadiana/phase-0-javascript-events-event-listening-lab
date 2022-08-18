18fd9565375c7f1419dbddf47fb8be26a44d3e65
monginadiana <dianahmongina2@gmail.com>
2022-08-18 22:40:27 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+// function addingEventListener(click) {
+// const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+// }
+
+// function addingEventListener() {
+//     const input = document.getElementById('input');
+//     input.addEventListener('click',()=>{console.log});
+// }
+
+
 function addingEventListener() {
+    const button = document.getElementById('button');
+    button.addEventListener('click', () => {console.log})
+    
 }
+ 
